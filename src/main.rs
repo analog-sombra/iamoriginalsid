20d7f165deea8b550347772950329672ea6fcfa7
analog-sombra <analogsombra@gmail.com>
2024-07-06 11:36:56 +0530

    refactor: client connection code

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,68 @@
 mod config;
+use std::process::exit;
+
+use log::{error, info};
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
-use simple_logger::SimpleLogger;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event. This is called whenever a new message is received.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple events can be
-    // dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an authentication error, or lack
-            // of permissions to post in the channel, so log to stdout when some error happens,
-            // with a description of it.
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
+                info!("Error sending message: {why:?}");
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
-    // a READY payload is sent by Discord. This payload contains data like the current user's guild
-    // Ids, current user data, private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        info!("{} is connected!", ready.user.name);
     }
 }
 
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new().init().unwrap();
+    env_logger::init();
+    // let logger = SimpleLogger::new().init();
+
+    // match logger {
+    //     Ok(_) => {
+    //         info!("Logger init!")
+    //     }
+    //     Err(e) => {
+    //         println!("{e}");
+    //         exit(1);
+    //     }
+    // }
 
     let config = (*config::config::CONFIG).clone();
 
-    // Configure the client with your Discord bot token in the environment.
     let token = config.token;
-    // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    // Create a new instance of the Client, logging in as a bot. This will automatically prepend
-    // your bot token with "Bot ", which is a requirement by Discord for bot users.
-    let mut client = Client::builder(&token, intents)
+    let client = Client::builder(&token, intents)
         .event_handler(Handler)
-        .await
-        .expect("Err creating client");
+        .await;
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform exponential backoff until
-    // it reconnects.
-    if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
+    match client {
+        Ok(mut bot) => match bot.start().await {
+            Ok(_) => {
+                info!("UwU I am Online");
+            }
+            Err(why) => {
+                error!("Client error: {why:?}");
+                exit(1);
+            }
+        },
+        Err(e) => {
+            println!("{e}");
+            exit(1);
+        }
     }
 }
