22a5683ab8335069d999e5a8bdcda89ad86e61dd
ExpertWasCoding <unheckableexpert@gmail.com>
2024-07-06 21:36:06 +0530

    feat: added age command, chore(deps): poise, feat(scope): migrated from serenity to feat

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,42 @@
+use poise::serenity_prelude as serenity;
 mod config;
-use std::process::exit;
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-use log::{error, info};
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
-//Initial commit
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                info!("Error sending message: {why:?}");
-            }
-        }
-    }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-    }
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-    // let logger = SimpleLogger::new().init();
-
-    // match logger {
-    //     Ok(_) => {
-    //         info!("Logger init!")
-    //     }
-    //     Err(e) => {
-    //         println!("{e}");
-    //         exit(1);
-    //     }
-    // }
-
     let config = (*config::config::CONFIG).clone();
-
     let token = config.token;
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = serenity::GatewayIntents::non_privileged();
 
-    let client = Client::builder(&token, intents)
-        .event_handler(Handler)
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
         .await;
-
-    match client {
-        Ok(mut bot) => match bot.start().await {
-            Ok(_) => {
-                info!("UwU I am Online");
-            }
-            Err(why) => {
-                error!("Client error: {why:?}");
-                exit(1);
-            }
-        },
-        Err(e) => {
-            println!("{e}");
-            exit(1);
-        }
-    }
+    client.unwrap().start().await.unwrap();
 }
